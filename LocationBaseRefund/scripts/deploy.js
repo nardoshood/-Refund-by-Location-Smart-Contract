7413d0b6e1084050c5bb11e14bca590f523aab76
nardoshood <nardostilahun83@gmail.com>
2022-07-18 00:38:14 +0300

    Deploy and verify deployment Script

--- LocationBaseRefund/scripts/deploy.js
+++ LocationBaseRefund/scripts/deploy.js
@@ -1,5 +1,6 @@
 // import
-const { ethers } = require("hardhat")
+const { verifyMessage, getContractAddress } = require("ethers/lib/utils")
+const { ethers, run, network } = require("hardhat")
 
 //asunc function
 async function main() {
@@ -7,8 +8,29 @@ async function main() {
     console.log("Deploying contract...")
     const location = await Location.deploy()
     await location.deployed()
-//Check delpoyment
+    //Check delpoyment
     console.log(`Deployed to: ${location.address}`)
+    if (network.config.chainId == 4 && process.env.ETHERSCAN_API_KEY) {
+        await location.deployTransaction.wait(4)
+        await verify(location.address, [])
+        console.log("Verified contract")
+    }
+}
+// auto verificaiton
+async function verify(getContractAddress, args) {
+    console.log("Verifying....")
+    try {
+        await run("verify:verify", {
+            address: constractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.messege_toLowerCase().includes("already Verified")) {
+            console.log("Already Verified")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 //main function
