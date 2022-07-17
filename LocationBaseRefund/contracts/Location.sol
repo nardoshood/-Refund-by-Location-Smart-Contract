a7263e7709535ea0c013a2505b26c5c805faecc6
nardoshood <nardostilahun83@gmail.com>
2022-07-18 01:25:54 +0300

    Smart contract updated to check location radius

--- LocationBaseRefund/contracts/Location.sol
+++ LocationBaseRefund/contracts/Location.sol
@@ -16,6 +16,7 @@ contract Location {
         uint256 latitude;
         uint256 longitude;
         string duration;
+        string raduis;
     }
 
     //List of properties
@@ -23,7 +24,6 @@ contract Location {
     stateType public state;
     address public employer;
     address[] public employees;
-
 
     constructor() {
         employer = msg.sender;
@@ -52,18 +52,54 @@ contract Location {
     function getEmployees() public view returns (address[] memory) {
         return employees;
     }
+
+    function getEmployeeDevice(address _address) public view
+        returns (
+            string memory,
+            uint256,
+            uint256,
+            string memory
+        )
+    {
+        return (
+            employeeDevice[_address].publicKey,
+            employeeDevice[_address].latitude,
+            employeeDevice[_address].longitude,
+            employeeDevice[_address].duration
     
-     function getEmployeeDevice(address _address) public view returns (string memory, uint256, uint256, string memory) {
-        return (employeeDevice[_address].publicKey, employeeDevice[_address].latitude, employeeDevice[_address].longitude, employeeDevice[_address].duration);
+        );
     }
-    
+
     // function getEmployer() public view returns(stateType memory) {
     //     return state;
 
     // }
+ function sqrt(uint256 input) private pure returns (uint256 output) {
+        uint256 interim = (input + 1) / 2;
+        output = input;
+        while (interim < output) {
+            output = interim;
+            interim = (input / output * 2) / 2;
+        }
+    }
+    function calculate_radius(
+        uint256 latitude,
+        uint256 longitude,
+        address _address
+    ) private view returns (uint256) {
+        uint256 radius = 0;
+        uint256 x = latitude - employeeDevice[_address].latitude;
+        uint256 y = longitude - employeeDevice[_address].latitude;
+        radius = sqrt(x**2 + y**2);
+        return radius;
+    }
 
-    // function calcDistance() publice view (){
-
-    // }
-    
+    function check_position(int256 lat, int256 lon) public {
+        uint256 new_radius = calculate_radius(latitude,  longitude, msg.sender);
+        if (new_radius < employeeDevice[msg.sender].radius) {
+            employeeDevice[msg.sender].status = true;
+        } else {
+            employeeDevice[msg.sender].status = false;
+        }
+    }
 }
